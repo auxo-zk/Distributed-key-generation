c28461cb5ed2b020615359de84eb53abf2f4f177
huyminh1115 <huyminh1115@gmail.com>
2024-01-15 01:07:16 +0700

    Update script create request

--- src/scripts/interactions/request/createRequest.ts
+++ src/scripts/interactions/request/createRequest.ts
@@ -1,180 +1,142 @@
-// import { Field, Mina, Provable, PublicKey } from 'o1js';
-// import {
-//   compile,
-//   fetchActions,
-//   fetchZkAppState,
-//   proveAndSend,
-// } from '../../helper/deploy.js';
-// import { prepare } from '../prepare.js';
-// import { DKGContract, KeyStatus, UpdateKey } from '../../../contracts/DKG.js';
-// import { CreateRequest, DKGAction, RequestContract } from '../../../index.js';
-// import { KeyStatusStorage } from '../../../contracts/DKGStorage.js';
-// import { KeyCounterStorage } from '../../../contracts/CommitteeStorage.js';
-// import axios from 'axios';
+import { Field, Mina, Provable, PublicKey, Group, Scalar } from 'o1js';
+import {
+  compile,
+  fetchActions,
+  fetchZkAppState,
+  proveAndSend,
+} from '../../helper/deploy.js';
+import { prepare } from '../prepare.js';
+import { DKGContract, KeyStatus, UpdateKey } from '../../../contracts/DKG.js';
+import { KeyStatusStorage } from '../../../contracts/DKGStorage.js';
+import { KeyCounterStorage } from '../../../contracts/CommitteeStorage.js';
+import {
+  RequestContract,
+  RequestInput,
+  UnRequestInput,
+  ResolveInput,
+  CreateRequest,
+  RequestVector,
+  RequestFee,
+  RollupStateOutput,
+  ActionEnum,
+  createActionMask,
+  RequestAction,
+  RequestStatusEnum,
+  RequestProof,
+  MockResponeContract,
+} from '../../../contracts/Request.js';
+import {
+  generateEncryptionWithRandomInput,
+  accumulateEncryption,
+} from '../../../libs/Requestor.js';
 
-// async function main() {
-//   const { cache, feePayer } = await prepare();
-//   const commiteeId = Field(0);
-//   const keyId = Field(0);
+import axios from 'axios';
 
-//   // Compile programs
-//   await compile(CreateRequest, cache);
-//   await compile(RequestContract, cache);
-//   const requestAddress = '';
-//   const requestContract = new RequestContract(
-//     PublicKey.fromBase58(requestAddress)
-//   );
+async function main() {
+  const { cache, feePayer } = await prepare();
 
-//   // Fetch storage trees
+  // Compile programs
+  await compile(CreateRequest, cache);
+  await compile(RequestContract, cache);
+  const requestAddress =
+    'B62qnDCCc8iHuXu7systFTc2EuipJQQcbA5DwYGXkJgrviv7dkcSnPi';
+  const requestContract = new RequestContract(
+    PublicKey.fromBase58(requestAddress)
+  );
 
-//   const committees = (await axios.get('https://api.auxo.fund/v0/committees/'))
-//     .data;
+  // Fetch storage trees
+  let keyCounterStorage = new KeyCounterStorage();
+  let keyStatusStorage = new KeyStatusStorage();
 
-//   const keys = await Promise.all(
-//     [...Array(committees.length).keys()].map(
-//       async (e) =>
-//         (
-//           await axios.get(`https://api.auxo.fund/v0/committees/${e}/keys`)
-//         ).data
-//     )
-//   );
-//   const keyCounters = keys.map((e) => e.length);
-//   keys.map((e, id) => {
-//     if (e.length == 0) return;
-//     keyCounterStorage.updateLeaf(
-//       KeyCounterStorage.calculateLeaf(Field(keyCounters[id])),
-//       KeyCounterStorage.calculateLevel1Index(Field(id))
-//     );
-//     e.map((key: any) => {
-//       keyStatusStorage.updateLeaf(
-//         Field(key.status),
-//         KeyStatusStorage.calculateLevel1Index({
-//           committeeId: Field(key.committeeId),
-//           keyId: Field(key.keyId),
-//         })
-//       );
-//     });
-//   });
+  const committees = (await axios.get('https://api.auxo.fund/v0/committees/'))
+    .data;
 
-//   // Fetch state and actions
-//   const rawState = (await fetchZkAppState(dkgAddress)) || [];
-//   const dkgState = {
-//     zkApps: rawState[0],
-//     keyCounter: rawState[1],
-//     keyStatus: rawState[2],
-//   };
+  const keys = await Promise.all(
+    [...Array(committees.length).keys()].map(
+      async (e) =>
+        (
+          await axios.get(`https://api.auxo.fund/v0/committees/${e}/keys`)
+        ).data
+    )
+  );
 
-//   const fromState =
-//     Field(
-//       25079927036070901246064867767436987657692091363973573142121686150614948079097n
-//     );
-//   const toState = Field(0n);
+  const keyCounters = keys.map((e) => e.length);
+  keys.map((e, id) => {
+    if (e.length == 0) return;
+    keyCounterStorage.updateLeaf(
+      KeyCounterStorage.calculateLeaf(Field(keyCounters[id])),
+      KeyCounterStorage.calculateLevel1Index(Field(id))
+    );
+    e.map((key: any) => {
+      keyStatusStorage.updateLeaf(
+        Field(key.status),
+        KeyStatusStorage.calculateLevel1Index({
+          committeeId: Field(key.committeeId),
+          keyId: Field(key.keyId),
+        })
+      );
+    });
+  });
 
-//   const rawActions = await fetchActions(
-//     dkgAddress,
-//     fromState
-//     // toState
-//   );
-//   const actions: DKGAction[] = rawActions.map((e) => {
-//     let action: Field[] = e.actions[0].map((e) => Field(e));
-//     return DKGAction.fromFields(action);
-//   });
-//   actions.map((e) => Provable.log(e));
+  console.log('Keys: ', keys);
 
-//   console.log('UpdateKey.firstStep...');
-//   let proof = await UpdateKey.firstStep(
-//     DKGAction.empty(),
-//     dkgState.keyCounter,
-//     dkgState.keyStatus,
-//     fromState
-//   );
-//   console.log('Done');
+  // Create request value
+  let committeeId = Field(0);
+  let keyId = Field(0);
+  let publicKey: Group;
+  let MINA = BigInt(1e7); // 0.01 MINA
+  let random: Scalar[] = [
+    Scalar.from(100n),
+    Scalar.from(200n),
+    Scalar.from(300n),
+  ];
 
-//   for (let i = 0; i < actions.length; i++) {
-//     let action = actions[i];
-//     Provable.log('Action:', action);
-//     if (action.keyId.equals(Field(-1)).toBoolean()) {
-//       console.log('UpdateKey.nextStepGeneration...');
-//       proof = await UpdateKey.nextStepGeneration(
-//         action,
-//         proof,
-//         Field(keyCounters[Number(action.committeeId)]),
-//         keyCounterStorage.getWitness(
-//           KeyCounterStorage.calculateLevel1Index(action.committeeId)
-//         ),
-//         keyStatusStorage.getWitness(
-//           KeyStatusStorage.calculateLevel1Index({
-//             committeeId: action.committeeId,
-//             keyId: Field(i),
-//           })
-//         )
-//       );
-//       console.log('Done');
+  let investInputs = [
+    [MINA, 2n * MINA, 3n * MINA],
+    [4n * MINA, 5n * MINA, 6n * MINA],
+    [7n * MINA, 8n * MINA, 9n * MINA],
+  ];
 
-//       keyStatusStorage.updateLeaf(
-//         Provable.switch(action.mask.values, Field, [
-//           Field(KeyStatus.ROUND_1_CONTRIBUTION),
-//           Field(KeyStatus.ROUND_2_CONTRIBUTION),
-//           Field(KeyStatus.ACTIVE),
-//           Field(KeyStatus.DEPRECATED),
-//         ]),
-//         KeyStatusStorage.calculateLevel1Index({
-//           committeeId: action.committeeId,
-//           keyId: Field(keyCounters[Number(action.committeeId)]),
-//         })
-//       );
+  let R: Group[][] = [];
+  let M: Group[][] = [];
 
-//       keyCounterStorage.updateLeaf(
-//         KeyCounterStorage.calculateLeaf(
-//           Field(++keyCounters[Number(action.committeeId)])
-//         ),
-//         KeyCounterStorage.calculateLevel1Index(action.committeeId)
-//       );
-//     } else {
-//       console.log('UpdateKey.nextStep...');
-//       proof = await UpdateKey.nextStep(
-//         action,
-//         proof,
-//         keyStatusStorage.getWitness(
-//           KeyStatusStorage.calculateLevel1Index({
-//             committeeId: action.committeeId,
-//             keyId: Field(0),
-//           })
-//         )
-//       );
+  for (let i = 0; i < investInputs.length; i++) {
+    let temp = generateEncryptionWithRandomInput(
+      random,
+      publicKey,
+      investInputs[i]
+    );
+    R.push(temp.R);
+    R.push(temp.M);
+  }
 
-//       keyStatusStorage.updateLeaf(
-//         Provable.switch(action.mask.values, Field, [
-//           Field(KeyStatus.ROUND_1_CONTRIBUTION),
-//           Field(KeyStatus.ROUND_2_CONTRIBUTION),
-//           Field(KeyStatus.ACTIVE),
-//           Field(KeyStatus.DEPRECATED),
-//         ]),
-//         KeyStatusStorage.calculateLevel1Index({
-//           committeeId: action.committeeId,
-//           keyId: Field(0),
-//         })
-//       );
-//       console.log('Done');
-//     }
-//   }
+  let totalValue = accumulateEncryption(R, M);
 
-//   let tx = await Mina.transaction(
-//     {
-//       sender: feePayer.key.publicKey,
-//       fee: feePayer.fee,
-//       nonce: feePayer.nonce++,
-//     },
-//     () => {
-//       dkgContract.updateKeys(proof);
-//     }
-//   );
-//   await proveAndSend(tx, feePayer.key, 'DKGContract', 'updateKeys');
-// }
+  let input: RequestInput = new RequestInput({
+    committeeId,
+    keyId,
+    R: RequestVector.from(totalValue.sumR),
+  });
 
-// main()
-//   .then()
-//   .catch((err) => {
-//     console.error(err);
-//     process.exit(1);
-//   });
+  // Fetch state and state
+  const rawState = (await fetchZkAppState(requestAddress)) || [];
+
+  let tx = await Mina.transaction(
+    {
+      sender: feePayer.key.publicKey,
+      fee: feePayer.fee,
+      nonce: feePayer.nonce++,
+    },
+    () => {
+      requestContract.request(input);
+    }
+  );
+  await proveAndSend(tx, feePayer.key, 'RequestContract', 'request');
+}
+
+main()
+  .then()
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
