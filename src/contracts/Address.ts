afa685e7b313f209ae960c9e7541532c7a5c46ea
phn210 <phamhonguyen210@gmail.com>
2024-02-20 23:26:49 +0100

    suggest sharing the use of zkapp address MT between contracts

---
+++ src/contracts/Address.ts
@@ -0,0 +1,32 @@
+import { Field, Poseidon, SmartContract, State, method, state } from 'o1js';
+import { EventEnum } from './shared.js';
+import { EMPTY_ADDRESS_MT, ZkAppRef } from './SharedStorage.js';
+
+export class AddressContract extends SmartContract {
+    // MT of other zkApp address
+    @state(Field) zkAppRoot = State<Field>();
+    @state(Field) actionState = State<Field>();
+
+    events = {
+        [EventEnum.ROLLUPED]: Field,
+    };
+
+    init() {
+        super.init();
+        this.zkAppRoot.set(EMPTY_ADDRESS_MT().getRoot());
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    @method updateAddress() {}
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    @method rollup() {}
+
+    verifyZkApp(ref: ZkAppRef) {
+        this.zkAppRoot
+            .getAndRequireEquals()
+            .assertEquals(
+                ref.witness.calculateRoot(Poseidon.hash(ref.address.toFields()))
+            );
+    }
+}
